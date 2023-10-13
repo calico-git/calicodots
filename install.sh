df60f4912c14a7ed6fbfb90268fd688e1f392ae3
calico <147751359+calico-git@users.noreply.github.com>
2023-10-14 01:23:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd ..
-yay -S kitty swaybg zsh wofi nano sddm steam discord thunderbird dolphin spotify firefox htop neofetch gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio
+yay -S kitty nwg-loo swaybg zsh wofi nano sddm steam discord thunderbird dolphin spotify firefox htop neofetch gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 sudo make install
